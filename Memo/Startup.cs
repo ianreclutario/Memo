7610b70e50419dbd5f1b1610e72df25c4a81bb09
ianre <ianre@DESKTOP-V5SA1I6>
2021-05-16 19:17:18 +0800

    Add project files.

---
+++ Memo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Memo.Startup))]
+namespace Memo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
