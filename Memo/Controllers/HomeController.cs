9a81ddcfaf712ad4b9c5bec1fd6d582d437b1f99
ianre <ianre@DESKTOP-V5SA1I6>
2021-05-16 23:24:49 +0800

    nagawa ko na yung convert, hindi lang pala loaded yung jquery

--- Memo/Controllers/HomeController.cs
+++ Memo/Controllers/HomeController.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Configuration;
+using System.Data.SqlClient;
 
 namespace Memo.Controllers
 {
+    //nagawa ko na putangina!!!!!!
     public class HomeController : Controller
     {
+        //get dbconnection
+        string connectionString = ConfigurationManager.ConnectionStrings["DBConnection"].ConnectionString;
         public ActionResult Index()
         {
             return View();
@@ -25,6 +30,29 @@ namespace Memo.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+
+        public JsonResult InsertFields(string firstName, string lastName, string choice)
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection(connectionString);
+                con.Open();
+                string query =  "INSERT INTO PERSON(FIRST_NAME, LAST_NAME, CHOICE) VALUES (@firstName, @lastName, @choice)";
+                SqlCommand cmd = new SqlCommand(query,con);
+                cmd.Parameters.AddWithValue("@firstName", firstName);
+                cmd.Parameters.AddWithValue("@lastName", lastName);
+                cmd.Parameters.AddWithValue("@choice", choice);
+                cmd.ExecuteNonQuery();
+                con.Close();
+
+                return Json("success", JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception e)
+            {
+                return Json("failure", JsonRequestBehavior.AllowGet);
+            }
         }
     }
 }
---
+++ Memo/Scripts/CRUD/crud.js
@@ -0,0 +1,40 @@
+﻿function Insert()
+{
+    var firstName = $('#FirstName').val();
+    var lastName = $('#LastName').val();
+    var choice = $('#Choice').val();
+
+
+    $.ajax({
+        type: "POST",
+        url: "/Home/InsertFields/",
+        datatype: "json",
+        contentType: "application/json: charset=utf-8",
+        data: JSON.stringify({ firstName: firstName, lastName: lastName, choice: choice }),
+        success: function (json) {
+
+        },
+        failure: function (errMsg) {
+            alert(errMsg);
+        }
+    })
+
+
+
+    
+}
+
+function convert() {
+    var amount = $('#Amount').val();
+    $('#Pesos').val(numberToWords(amount));
+}
+
+function numberToWords(number) {
+    var digit = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+    var elevenSeries = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    var countingByTens = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    var shortScale = ['', 'thousand', 'million', 'billion', 'trillion'];
+
+    number = number.toString(); number = number.replace(/[\, ]/g, ''); if (number != parseFloat(number)) return 'not a number'; var x = number.indexOf('.'); if (x == -1) x = number.length; if (x > 15) return 'too big'; var n = number.split(''); var str = ''; var sk = 0; for (var i = 0; i < x; i++) { if ((x - i) % 3 == 2) { if (n[i] == '1') { str += elevenSeries[Number(n[i + 1])] + ' '; i++; sk = 1; } else if (n[i] != 0) { str += countingByTens[n[i] - 2] + ' '; sk = 1; } } else if (n[i] != 0) { str += digit[n[i]] + ' '; if ((x - i) % 3 == 0) str += 'hundred '; sk = 1; } if ((x - i) % 3 == 1) { if (sk) str += shortScale[(x - i - 1) / 3] + ' '; sk = 0; } } if (x != number.length) { var y = number.length; str += 'point '; for (var i = x + 1; i < y; i++) str += digit[n[i]] + ' '; } str = str.replace(/\number+/g, ' '); return str.trim() + " pesos ONLY";
+
+}
